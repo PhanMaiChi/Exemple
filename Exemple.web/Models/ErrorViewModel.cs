7aa814316341c56015db00560e991b462b61b0e2
unname <phantrung2510@gmail.com>
2021-05-20 08:50:42 +0700

    Add project files.

---
+++ Exemple.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exemple.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
